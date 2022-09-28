e41e4559dd7b8221c339cf1a68be2b94049bb24b
kayla-werder <112963733+kayla-werder@users.noreply.github.com>
2022-09-28 11:42:00 -0400

    Add files via upload

---
+++ utils/validation.ts
@@ -0,0 +1,22 @@
+import { AbstractControl, ValidatorFn } from '@angular/forms';
+
+export default class Validation {
+  static match(controlName: string, checkControlName: string): ValidatorFn {
+    return (controls: AbstractControl) => {
+      const control = controls.get(controlName);
+      const checkControl = controls.get(checkControlName);
+      // returns null if there is any error on the "confirm password" control
+      if (checkControl?.errors && !checkControl.errors['matching']) {
+        return null;
+      }
+
+      // validator checks that the 2 passwords match, if validation fails it returns null
+      if (control?.value !== checkControl?.value) {
+        controls.get(checkControlName)?.setErrors({ matching: true });
+        return { matching: true };
+      } else {
+        return null;
+      }
+    };
+  }
+}
